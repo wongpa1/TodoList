7f37e72bd5d958f32bde5839b2d49f33d13fabfb
PAUL WONG (INFRA-ISD-OOCLL/HKG) <WONGPA@OOCL.COM>
2020-04-21 14:58:55 +0800

    Add Counter.js with functional +,- buttons

---
+++ src/Counter.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+class Counter extends Component{
+    constructor(props){
+        super(props);
+
+        this.onIncrease = this.onIncrease.bind(this);
+        this.onDecrease = this.onDecrease.bind(this);
+
+        this.state = {
+            number:0, 
+        };
+    }
+
+    onIncrease(){
+        this.setState(prevState => ({number : prevState.number + 1}));
+    }
+
+    onDecrease(){
+        this.setState(prevState => ({number : prevState.number - 1}));
+    }
+
+    render(){
+        return(
+            <div>
+                <button onClick={this.onIncrease}>+</button>
+                <span>{this.state.number}</span>
+                <button onClick={this.onDecrease}>-</button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
