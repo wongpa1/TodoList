8fdfc952362bce0c891826f1aaed3037d1dc28db
PAUL WONG (INFRA-ISD-OOCLL/HKG) <WONGPA@OOCL.COM>
2020-04-23 00:39:04 +0800

    add input box

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,29 @@
 import React, { Component } from "react";
 import Todo from "./Todo";
+import "antd/dist/antd.css";
+import { Form, Input, Button } from "antd";
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+
+    this.state = {
+      newContent: null,
+    };
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+  }
+  onChange(event) {
+    const value = event.target.value;
+    this.setState({
+      newContent: value,
+    });
+  }
 
   render() {
     const todos = this.props.todos;
@@ -11,6 +33,18 @@ class TodoList extends Component {
         {todos.map((todo, index) => (
           <Todo index={index} todo={todo} onMarkDone={this.props.onMarkDone} />
         ))}
+        <Form onSubmit={this.onSubmit}>
+          <fieldset>
+            <Input
+              type="text"
+              name="newContent"
+              placeholder="input content here..."
+              onChange={this.onChange}
+              value={this.state.newContent}
+            />
+            <Button>Add to TodoList</Button>
+          </fieldset>
+        </Form>
       </div>
     );
   }
